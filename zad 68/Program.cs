5e126e210ad9603a5f3aabd8b17b2426b03c6e8a
leysan <leis90@list.ru>
2023-01-09 01:14:37 +0300

    2

---
+++ zad 68/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+Console.Write("Введите натуральное число M: ");
+uint m = Convert.ToUInt32(Console.ReadLine());
+Console.Write("Введите натуральное число N: ");
+uint n = Convert.ToUInt32(Console.ReadLine());
+Console.WriteLine(Acker(m, n));
+
+uint Acker(uint m, uint n)
+{
+    if (m == 0) return n + 1;
+      if (m != 0 && n == 0)return Acker(m - 1, 1);
+       return Acker(m - 1, Acker(m, n - 1));
+}
+
+
---
+++ zad66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите натуральное число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число N большее M: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumNumbers(M, N));
+
+int SumNumbers(int M, int N)
+{
+    if (M == N) return N;
+    else return M + (SumNumbers(M + 1, N));
+}
